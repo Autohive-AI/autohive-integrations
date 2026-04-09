6f34bfaea2cde43fd66a1d0dfa9a095041afc8f4
Shubhank <72601061+Sagsgit@users.noreply.github.com>
2026-04-09 13:58:52 +1200

    fix(productboard): fix ruff format in context.py

--- productboard/tests/context.py
+++ productboard/tests/context.py
@@ -2,7 +2,7 @@
 import sys
 import os
 
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, parent_dir)
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../dependencies")))
 
